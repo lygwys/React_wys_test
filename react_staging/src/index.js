def21e007103018076692d97e9ae5e851215cb5a
lygwys <lygwys@outlook.com>
2021-07-05 20:51:38 +0800

    51 执行顺序：src/index.js引入各种库->去public/index.html找root->渲染App.js

--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  //<React.StrictMode>语法检查
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals()记录页面性能用到web-vitals库
 reportWebVitals();
+//其它文件：
+//setupTests.js组件单元测试用到jest-dom
+//App.test.js给App做测试
+//执行顺序：
+//index.js引入各种库->去public/index.html找root->渲染App.js
