fb188979ed8b02b97a7548c8905bb78d2e9b3f94
lygwys <lygwys@outlook.com>
2021-07-06 21:29:15 +0800

    拆分Itim组件

--- react_staging/src/components/List/index.jsx
+++ react_staging/src/components/List/index.jsx
@@ -1,23 +1,11 @@
 import React, { Component } from 'react'
+import Item from '../Item'
 
 export default class List extends Component {
     render() {
         return (
             <ul className="todo-main">
-                <li>
-                <label>
-                    <input type="checkbox"/>
-                    <span>xxxxx</span>
-                </label>
-                <button className="btn btn-danger" style={{display:'none'}}>删除</button>
-                </li>
-                <li>
-                <label>
-                    <input type="checkbox"/>
-                    <span>yyyy</span>
-                </label>
-                <button className="btn btn-danger" style={{display:'none'}}>删除</button>
-                </li>
+                <Item/>
             </ul>
         )
     }
