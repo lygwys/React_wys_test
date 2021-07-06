2115641e3f1d5d6e3a0ca930ad9256d0d7e41ca5
lygwys <lygwys@outlook.com>
2021-07-06 21:02:50 +0800

    56 TodoList案例_1.html整体迁移至react:

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
+import './App.css'
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                <h2 style={{fontSize:'40px'}}>App...</h2>
+            <div className="todo-container">
+                <div className="todo-wrap">
+                    <div className="todo-header">
+                        <input type="text" placeholder="请输入你的任务名称，按回车键确认"/>
+                    </div>
+                    <ul className="todo-main">
+                        <li>
+                        <label>
+                            <input type="checkbox"/>
+                            <span>xxxxx</span>
+                        </label>
+                        <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+                        </li>
+                        <li>
+                        <label>
+                            <input type="checkbox"/>
+                            <span>yyyy</span>
+                        </label>
+                        <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+                        </li>
+                    </ul>
+                    <div className="todo-footer">
+                        <label>
+                        <input type="checkbox"/>
+                        </label>
+                        <span>
+                        <span>已完成0</span> / 全部2
+                        </span>
+                        <button className="btn btn-danger">清除已完成任务</button>
+                    </div>
+                </div>
             </div>
         )
     }
