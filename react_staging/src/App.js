4da00d52a09e5b7a660ab46d6467da2b32a835e1
lygwys <lygwys@outlook.com>
2021-07-06 19:14:20 +0800

    55 App.js    index.js

---
+++ react_staging/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2 style={{fontSize:'40px'}}>App...</h2>
+            </div>
+        )
+    }
+}
