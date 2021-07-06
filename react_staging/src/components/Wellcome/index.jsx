7858f059a77f2e14ed6b2705d61b9cbc15c834f2
lygwys <lygwys@outlook.com>
2021-07-06 09:12:55 +0800

    当模块中样式名称相同时，先引入的被后引入的覆盖

--- react_staging/src/components/Wellcome/index.jsx
+++ react_staging/src/components/Wellcome/index.jsx
@@ -2,6 +2,6 @@ import react,{Component} from 'react'
 import './index.css'
 export default class Wellcome extends Component{
     render(){
-        return <h1 className="title2">Wellcome!</h1>
+        return <h1 className="title">Wellcome!</h1>
     }
 }
