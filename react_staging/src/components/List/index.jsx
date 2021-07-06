128d623e3dd6e3a347446d90a4c36a1a4712cbea
lygwys <lygwys@outlook.com>
2021-07-06 21:21:34 +0800

    完成List footer组件的拆分

--- react_staging/src/components/List/index.jsx
+++ react_staging/src/components/List/index.jsx
@@ -3,9 +3,22 @@ import React, { Component } from 'react'
 export default class List extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <ul className="todo-main">
+                <li>
+                <label>
+                    <input type="checkbox"/>
+                    <span>xxxxx</span>
+                </label>
+                <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+                </li>
+                <li>
+                <label>
+                    <input type="checkbox"/>
+                    <span>yyyy</span>
+                </label>
+                <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+                </li>
+            </ul>
         )
     }
 }
