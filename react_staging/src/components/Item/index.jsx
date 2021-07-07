ba95324c1a69578845e9bc0f59b7956ec0c7c1f1
lygwys <lygwys@outlook.com>
2021-07-07 08:54:52 +0800

    057 动态初始化列表

--- react_staging/src/components/Item/index.jsx
+++ react_staging/src/components/Item/index.jsx
@@ -3,11 +3,13 @@ import './index.css'
 
 export default class Item extends Component {
     render() {
+        const {name,done}=this.props
         return (
             <li>
                 <label>
-                    <input type="checkbox"/>
-                    <span>xxxxx</span>
+                    <input type="checkbox" defaultChecked={done}/>
+                    {/* 此处如果用checked就报错必须使用onChange    有bug */}
+                    <span>{name}</span>
                 </label>
                 <button className="btn btn-danger" style={{display:'none'}}>删除</button>
             </li>
