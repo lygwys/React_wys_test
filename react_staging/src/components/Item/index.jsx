a7d59879c09693538b55557bbdbef0db7f8e3c97
lygwys <lygwys@outlook.com>
2021-07-07 16:45:35 +0800

    059 鼠标移入效果 高亮

--- react_staging/src/components/Item/index.jsx
+++ react_staging/src/components/Item/index.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Item extends Component {
+    state ={mouse:false}
+    handleMouse=(flag)=>{
+        return ()=>{
+            this.setState({mouse:flag})
+        }
+    }
     render() {
         const {name,done}=this.props
+        const {mouse}=this.state
         return (
-            <li>
+            <li style={{backgroundColor:mouse?'#ddd':'white'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
                 <label>
                     <input type="checkbox" defaultChecked={done}/>
                     {/* 此处如果用checked就报错必须使用onChange    有bug */}
