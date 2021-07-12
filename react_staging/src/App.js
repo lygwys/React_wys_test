8dd0f9aa043ec1d01d56763eaf4fbb784c63a2fd
lygwys <lygwys@outlook.com>
2021-07-12 15:56:29 +0800

    65 脚手架配置代理_方法一

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
+    getStudentData = ()=>{
+        axios.get('http://localhost:3000/students').then(
+            response => {console.log('返回数据成功了：',response.data);},
+            error => {console.log('返回数据失败了：',error);}
+        )
+    }
     render() {
         return (
-            <h1>App...</h1>
+            <div>
+                <button onClick={this.getStudentData}>获取学生数据</button>
+            </div>
+
         )
     }
 }
