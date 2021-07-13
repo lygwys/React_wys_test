54eeb7bb06160f3df744b7bef7915b3629831b08
lygwys <lygwys@outlook.com>
2021-07-13 13:58:10 +0800

    连续解构赋值并重命名

--- react_staging/src/components/Search/index.jsx
+++ react_staging/src/components/Search/index.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 
 export default class Search extends Component {
+    search = () =>{
+        const {keyWordElement:{value:keyWord}}=this
+        console.log(keyWord)
+
+    }
     render() {
         return (
             <section className="jumbotron">
-                <h3 className="jumbotron-heading">Search Github Users</h3>
+                <h3 className="jumbotron-heading">搜索github用户</h3>
                 <div>
-                    <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+                    <input ref = {c =>this.keyWordElement = c} type="text" placeholder="输入关键词点击搜索"/>&nbsp;
+                    <button onClick={this.search}>搜索</button>
                 </div>
             </section>
         )
