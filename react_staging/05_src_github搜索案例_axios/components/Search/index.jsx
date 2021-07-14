533ca87a9e3a64367284b02b3f9692ded85f391c
lygwys <lygwys@outlook.com>
2021-07-14 15:58:41 +0800

    备份 05_src_github搜索案例_axios

---
+++ react_staging/05_src_github搜索案例_axios/components/Search/index.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class Search extends Component {
+    search = () =>{
+        const {keyWordElement:{value:keyWord}}=this
+        this.props.updateAppState({isFirst:false,isLoading:true})
+        axios.get(`/api1/search/users?q=${keyWord}`).then(
+            response=>{
+                this.props.updateAppState({isLoading:false,users:response.data.items})
+            },
+            error=>{
+                this.props.updateAppState({isLoading:false,err:error.message})
+            }
+        )
+
+    }
+    render() {
+        return (
+            <section className="jumbotron">
+                <h3 className="jumbotron-heading">搜索github用户</h3>
+                <div>
+                    <input ref = {c =>this.keyWordElement = c} type="text" placeholder="输入关键词点击搜索"/>&nbsp;
+                    <button onClick={this.search}>搜索</button>
+                </div>
+            </section>
+        )
+    }
+}
