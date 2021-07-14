57c446295cc81204f612df18b90a043763598724
lygwys <lygwys@outlook.com>
2021-07-14 08:57:43 +0800

    state中的数据传给List组件

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -10,10 +10,11 @@ export default class App extends Component {
     }
 
     render() {
+        const {users} = this.state
         return (
             <div className="container">
                 {<Search saveUsers={this.saveUsers}/>}
-                {<List/>}
+                {<List users={users}/>}
           </div>
         )
     }
