f2a2a1e9bcb0793a50536f2ebb8ff92e26d56721
lygwys <lygwys@outlook.com>
2021-07-14 21:54:00 +0800

    初始化项目

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
-import Search from './components/Search'
-import List from './components/List'
+
 
 export default class App extends Component {
     render() {
         return (
-            <div className="container">
-                {<Search/>}
-                {<List/>}
-          </div>
+            <div>
+                App...        
+            </div>
         )
     }
 }
