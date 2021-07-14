24ca720fd85964e2d769fad63bd6b97adb477a3f
lygwys <lygwys@outlook.com>
2021-07-14 09:14:42 +0800

    列表展示

--- react_staging/src/components/List/index.jsx
+++ react_staging/src/components/List/index.jsx
@@ -5,36 +5,18 @@ export default class List extends Component {
     render() {
         return (
             <div className="row">
-                <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                    <img alt="head_portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
-                    </a>
-                    <p className="card-text">reactjs</p>
-                </div>
-                <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                    <img alt="head_portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
-                    </a>
-                    <p className="card-text">reactjs</p>
-                </div>
-                <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                    <img alt="head_portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
-                    </a>
-                    <p className="card-text">reactjs</p>
-                </div>
-                <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                    <img alt="head_portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
-                    </a>
-                    <p className="card-text">reactjs</p>
-                </div>
-                <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                    <img alt="head_portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
-                    </a>
-                    <p className="card-text">reactjs</p>
-                </div>
+                {
+                    this.props.users.map((userObj)=>{
+                        return (
+                            <div key={userObj.id} className="card">
+                                <a href={userObj.html_url} target="_blank" rel="noreferrer">
+                                <img alt="head_portrait" src={userObj.avatar_url} style={{width: '100px'}}/>
+                                </a>
+                                <p className="card-text">{userObj.login}</p>
+                            </div>
+                        )
+                    })
+                }
             </div>
             
         )
