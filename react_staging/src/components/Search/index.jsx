ab08afa21d31266b782b62df8134560c51e022ba
lygwys <lygwys@outlook.com>
2021-07-14 19:44:40 +0800

    72 fetch发送请求

--- react_staging/src/components/Search/index.jsx
+++ react_staging/src/components/Search/index.jsx
@@ -1,21 +1,61 @@
 import React, { Component } from 'react'
 import PubSub from 'pubsub-js'
-import axios from 'axios'
+//import axios from 'axios'
 
 export default class Search extends Component {
-    search = () =>{        
+    search = async() =>{        
         const {keyWordElement:{value:keyWord}}=this
         PubSub.publish('atguigu',{isFirst:false,isLoading:true})
-        axios.get(`/api1/search/users?q=${keyWord}`).then(
+        //#region 使用axios发送网络请求
+        /*axios.get(`/api1/search/users2?q=${keyWord}`).then(
             response=>{
                 PubSub.publish('atguigu',{isLoading:false,users:response.data.items})
             },
             error=>{
                 PubSub.publish('atguigu',{isLoading:false,err:error.message})
             }
-        )
+        )*/
+        //#endregion
+
+        //#region 使用fetch发送网络请求（未优化）
+        /*fetch(`/api1/search/users2?q=${keyWord}`).then(
+            response=>{
+                console.log('联系服务器成功');
+                return response.json()
+            },
+            // error=>{                
+            //     console.log('联系服务器失败',error) 
+            //     return new Promise(()=>{}) //返回初始化状态Promise实例，不再执行后面的then  
+            // }
+        ).then(
+            response=>{console.log('获取数据成功',response);},
+            //error=>{console.log('获取数据失败',error)}
+        ).catch(
+            (error)=>{console.log('请求出错',error);}//不用上面注释掉的内容，兜底输出错误
+        )*/
+        //#endregion
+
+
+        //#region 使用fetch发送网络请求（优化,前面最近的函数加async）
+        /*try {
+            const response=await fetch(`/api1/search/users2?q=${keyWord}`)
+            const data=await response.json()
+            console.log(data)
+        } catch (error) {
+            console.log('请求出错',error);
+        } */
+        //#endregion
+
+        try {
+            const response=await fetch(`/api1/search/users2?q=${keyWord}`)
+            const data=await response.json()
+            PubSub.publish('atguigu',{isLoading:false,users:data.items})
+        } catch (error) {
+            PubSub.publish('atguigu',{isLoading:false,err:error.message})
+        }
 
     }
+
     render() {
         return (
             <section className="jumbotron">
@@ -28,3 +68,4 @@ export default class Search extends Component {
         )
     }
 }
+
