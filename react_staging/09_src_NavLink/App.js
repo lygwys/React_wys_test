19010f1d9bfaf2aa9dbc69352bda38193a6450c8
lygwys <lygwys@outlook.com>
2021-07-16 08:06:29 +0800

    备份09_src_NavLink

---
+++ react_staging/09_src_NavLink/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import {NavLink,Route} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Header from './components/Header'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="row">
+                <div className="col-xs-offset-2 col-xs-8">
+                    <Header/>
+                </div>
+                </div>
+                <div className="row">
+                <div className="col-xs-2 col-xs-offset-2">
+                    <div className="list-group">
+                    {/* <a className="list-group-item" href="./about.html">About</a>
+                    <a className="list-group-item active" href="./home.html">Home</a> */}
+                        <NavLink activeClassName="atguigu" className="list-group-item" to="/home">Home</NavLink>
+                        <NavLink activeClassName="atguigu" className="list-group-item" to="/about">About</NavLink>                        
+                    </div>
+                </div>
+                <div className="col-xs-6">
+                    <div className="panel">
+                    <div className="panel-body">
+                            <Route path="/about" component={About}/>
+                            <Route path="/home" component={Home}/>                   
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        )
+    }
+}
