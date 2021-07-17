0483881f5f2b1fa79b1db7d48ca4e828d7078ec8
lygwys <lygwys@outlook.com>
2021-07-17 11:16:14 +0800

    这是Redirect的正确写法，前次保存有误

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import {Route,Switch,Rdiret} from 'react-router-dom'
+import {Route,Switch,Redirect} from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
 import Header from './components/Header'
 import MyNavLink from './components/MyNavLink'
-import { Redirect } from 'react-router-dom/cjs/react-router-dom.min'
 
 export default class App extends Component {
     render() {
@@ -18,13 +17,8 @@ export default class App extends Component {
                 <div className="row">
                 <div className="col-xs-2 col-xs-offset-2">
                     <div className="list-group">
-                    {/* <a className="list-group-item" href="./about.html">About</a>
-                    <a className="list-group-item active" href="./home.html">Home</a> */}
-                        {/* <NavLink activeClassName="atguigu" className="list-group-item" to="/home">Home</NavLink>
-                        <NavLink activeClassName="atguigu" className="list-group-item" to="/about">About</NavLink>  */}
                         <MyNavLink to="/home">Home</MyNavLink>
-                        <MyNavLink to="/about">About</MyNavLink>
-                        <Redirect to="about"/>
+                        <MyNavLink to="/about">About</MyNavLink>                        
                     </div>
                 </div>
                 <div className="col-xs-6">
@@ -33,6 +27,7 @@ export default class App extends Component {
                             <Switch>
                                 <Route path="/about" component={About}/>
                                 <Route path="/home" component={Home}/> 
+                                <Redirect to="about"/>
                             </Switch>              
                     </div>
                     </div>
