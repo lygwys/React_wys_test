4febf25ac7f900885fa884d896e9a486339d0b93
lygwys <lygwys@outlook.com>
2021-07-17 16:48:50 +0800

    87  向路由组件传递search参数

--- react_staging/src/pages/Home/Message/index.jsx
+++ react_staging/src/pages/Home/Message/index.jsx
@@ -19,14 +19,16 @@ export default class index extends Component {
                        messageArr.map((msgObj)=>{
                         return(
                             <li key={msgObj.id}>
-                                <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>
+                                {/* <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link> */}
+                                <Link to={`/home/message/detail/?id=${msgObj.id}&title=${msgObj.title}`}>{msgObj.title}</Link>
                             </li> 
                         )
                        }) 
                     }
                 </ul>
                 <hr />
-                <Route path="/home/message/detail/:id/:title" component={Detail}/>
+                {/* <Route path="/home/message/detail/:id/:title" component={Detail}/> */}
+                <Route path="/home/message/detail/" component={Detail}/>
             </div>
         )
     }
