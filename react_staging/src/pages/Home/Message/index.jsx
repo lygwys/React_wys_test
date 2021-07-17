d50d4e5576691e380c075ad2c33556ccb69a22a6
lygwys <lygwys@outlook.com>
2021-07-17 14:49:46 +0800

    改成动态数据

--- react_staging/src/pages/Home/Message/index.jsx
+++ react_staging/src/pages/Home/Message/index.jsx
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
 
 export default class index extends Component {
+    state={
+        messageArr:[
+            {id:'01',title:'消息1'},
+            {id:'02',title:'消息2'},
+            {id:'03',title:'消息3'},
+        ]
+    }
     render() {
+        const {messageArr}=this.state
         return (
             <div>
                 <ul>
-                    <li>
-                        <a href="/message1">message001</a>&nbsp;&nbsp;
-                    </li>
-                    <li>
-                        <a href="/message2">message002</a>&nbsp;&nbsp;
-                    </li>
-                    <li>
-                        <a href="/message/3">message003</a>&nbsp;&nbsp;
-                    </li>
+                    {
+                       messageArr.map((messObj)=>{
+                        return(
+                            <li key={messObj.id}>
+                                <a href="/???">{messObj.title}</a>&nbsp;&nbsp;
+                            </li> 
+                        )
+                       }) 
+                    }
                 </ul>
             </div>
         )
