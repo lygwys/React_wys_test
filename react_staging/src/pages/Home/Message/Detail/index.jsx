d9bc451bfabced26331e68f42e8f7c6fe078be9a
lygwys <lygwys@outlook.com>
2021-07-17 15:12:29 +0800

    提供详情数据DetaiData

--- react_staging/src/pages/Home/Message/Detail/index.jsx
+++ react_staging/src/pages/Home/Message/Detail/index.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
 
+const DetaiData= [
+    {id:'01',content:'你好，中国'},
+    {id:'02',content:'你好，尚硅谷'},
+    {id:'03',content:'你好，未来的自己'},
+]
+
 export default class index extends Component {
     render() {
         return (
