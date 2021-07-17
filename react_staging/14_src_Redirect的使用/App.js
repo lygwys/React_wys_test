8b211164ed09943671dc8b65626c2bd4aa5936bd
lygwys <lygwys@outlook.com>
2021-07-17 09:52:12 +0800

    备份14_src_Redirect的使用

---
+++ react_staging/14_src_Redirect的使用/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import {Route,Switch,Rdiret} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Header from './components/Header'
+import MyNavLink from './components/MyNavLink'
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="row">
+                <div className="col-xs-offset-2 col-xs-8">
+                    <Header/>
+                </div>
+                </div>
+                <div className="row">
+                <div className="col-xs-2 col-xs-offset-2">
+                    <div className="list-group">
+                    {/* <a className="list-group-item" href="./about.html">About</a>
+                    <a className="list-group-item active" href="./home.html">Home</a> */}
+                        {/* <NavLink activeClassName="atguigu" className="list-group-item" to="/home">Home</NavLink>
+                        <NavLink activeClassName="atguigu" className="list-group-item" to="/about">About</NavLink>  */}
+                        <MyNavLink to="/home">Home</MyNavLink>
+                        <MyNavLink to="/about">About</MyNavLink>
+                        <Redirect to="about"/>
+                    </div>
+                </div>
+                <div className="col-xs-6">
+                    <div className="panel">
+                    <div className="panel-body">
+                            <Switch>
+                                <Route path="/about" component={About}/>
+                                <Route path="/home" component={Home}/> 
+                            </Switch>              
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        )
+    }
+}
