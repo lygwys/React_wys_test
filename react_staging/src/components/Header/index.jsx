afebbd90542a7d274787c9a3c70e0370152eeae3
lygwys <lygwys@outlook.com>
2021-07-18 21:35:23 +0800

    92 withRouter的使用_将一般组件加工成路由组件

--- react_staging/src/components/Header/index.jsx
+++ react_staging/src/components/Header/index.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import {withRouter} from 'react-router-dom'
 
-export default class index extends Component {
+class index extends Component {
+    back = ()=>{
+        this.props.history.goBack()
+    }
+    forward = ()=>{
+        this.props.history.goForward()
+    }
+    go = ()=>{
+        this.props.history.go(2)
+    }
     render() {
         return (            
             <div className="page-header">
                 <h2>React Router Demo!</h2>
+
+                <button onClick={this.back}>后退</button>&nbsp;
+                <button onClick={this.forward}>前进</button>&nbsp;
+                <button onClick={this.go}>go +2</button>
+
             </div>
         )
     }
 }
+
+export default  withRouter(index)
