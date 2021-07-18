4d78eabb8f510b19798f20e5e2bb0dc2882761e1
lygwys <lygwys@outlook.com>
2021-07-18 09:07:23 +0800

    push按钮查看可以留下痕迹

--- react_staging/src/pages/Home/Message/index.jsx
+++ react_staging/src/pages/Home/Message/index.jsx
@@ -14,6 +14,9 @@ export default class index extends Component {
     replaceShow=(id,title)=>{
         this.props.history.replace(`/home/message/detail/${id}/${title}`)
     }
+    pushShow=(id,title)=>{
+        this.props.history.push(`/home/message/detail/${id}/${title}`)
+    }
 
     render() {
         const {messageArr}=this.state
@@ -25,7 +28,7 @@ export default class index extends Component {
                         return(
                             <li key={msgObj.id}>
                                 <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>
-                                &nbsp;<button>push查看</button>
+                                &nbsp;<button onClick={()=>this.pushShow(msgObj.id,msgObj.title)}>push查看</button>
                                 &nbsp;<button onClick={()=>this.replaceShow(msgObj.id,msgObj.title)}>replace查看</button>
                                 {/* <Link to={`/home/message/detail/?id=${msgObj.id}&title=${msgObj.title}`}>{msgObj.title}</Link> */}
                                 {/* <Link to={{pathname:'/home/message/detail',state:{id:msgObj.id,title:msgObj.title}}}>{msgObj.title}</Link> */}
