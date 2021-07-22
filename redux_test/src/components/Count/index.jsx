485aa8dde266a732bc2b8a670b832085c956b6fa
lygwys <lygwys@outlook.com>
2021-07-22 10:55:21 +0800

    改造UI组件

--- redux_test/src/components/Count/index.jsx
+++ redux_test/src/components/Count/index.jsx
@@ -1,38 +1,24 @@
 import React, { Component } from 'react'
-import store from '../../redux/store'
-import {createDecrementAction,
-    createIncrementAction,
-    createIncrementAsyncAction
-} from '../../redux/count_action'
-
 
 export default class index extends Component {
     state={carName:'奔驰c63'}
 
     increment=()=>{
         const {value}=this.selectNumber
-        store.dispatch(createIncrementAction(value*1))
     }
     decrement=()=>{
         const {value}=this.selectNumber
-        store.dispatch(createDecrementAction(value*1))
     }
     incrementIfOdd=()=>{
         const {value}=this.selectNumber
-        const count=store.getState()
-        if (count % 2 !== 0) {
-            store.dispatch(createIncrementAction(value*1))
-        }
     }
     incrementAsync=()=>{
         const {value}=this.selectNumber
-        const count=store.getState()
-        store.dispatch(createIncrementAsyncAction(value*1,500))
     }
     render() {
         return (
             <div>
-                <h1>当前求和为：{store.getState()}</h1>
+                <h1>当前求和为：???</h1>
                 <select ref={c=>this.selectNumber=c}>
                     <option value="1">1</option>
                     <option value="2">2</option>
