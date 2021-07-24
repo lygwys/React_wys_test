640c49f8642bc221d03a1cd41c3c2e3132dc32a8
lygwys <lygwys@outlook.com>
2021-07-24 12:10:14 +0800

    1.静态页

---
+++ redux_test/src/containers/Count/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <h2>当前求和为：？？？</h2>
+            </div>
+        )
+    }
+}
