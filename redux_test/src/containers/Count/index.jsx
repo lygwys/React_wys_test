5d9a13a056334f9c76799ecc1314496aba37ccb1
lygwys <lygwys@outlook.com>
2021-07-24 08:34:40 +0800

    函数也不要了，直接将函数体内容写到相应参数位置

--- redux_test/src/containers/Count/index.jsx
+++ redux_test/src/containers/Count/index.jsx
@@ -7,12 +7,13 @@ import {createIncrementAction,
 
 const mapStateToProps = state => ({count:state})
 
-const mapDispatchToProps = dispatch =>(
-    {
-        jia:number=>dispatch(createIncrementAction(number)),
-        jian:number=>dispatch(createDecrementAction(number)),
-        jiaAsync:(number,time)=>dispatch(createIncrementAsyncAction(number,time)),
-    }
-)
-
-export default connect(mapStateToProps,mapDispatchToProps)(CountUI)
+export default connect(
+    state => ({count:state}),
+    dispatch =>(
+        {
+            jia:number=>dispatch(createIncrementAction(number)),
+            jian:number=>dispatch(createDecrementAction(number)),
+            jiaAsync:(number,time)=>dispatch(createIncrementAsyncAction(number,time)),
+        }
+    )
+)(CountUI)
