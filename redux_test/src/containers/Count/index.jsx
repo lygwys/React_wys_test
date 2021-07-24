621cba04061ac04e8c2795f9922d6ff0ea690695
lygwys <lygwys@outlook.com>
2021-07-24 12:29:52 +0800

    完成练习，重新替换为优化版

--- redux_test/src/containers/Count/index.jsx
+++ redux_test/src/containers/Count/index.jsx
@@ -1,22 +1,56 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import {createIncrementAction} from '../../redux/count_action'
+import {createIncrementAction,
+    createDecrementAction,
+    createIncrementAsyncAction
+} from '../../redux/count_action'
 
 class Count extends Component {
-    add = ()=>{
-        this.props.add(1)
+    state={carName:'奔驰c63'}
+
+    increment=()=>{
+        const {value}=this.selectNumber
+        this.props.jia(value*1)
+    }
+    decrement=()=>{
+        const {value}=this.selectNumber
+        this.props.jian(value*1)
+    }
+    incrementIfOdd=()=>{
+        const {value}=this.selectNumber
+        if (this.props.count % 2 !== 0) {
+            this.props.jia(value*1)
+        }
+    }
+    incrementAsync=()=>{
+        const {value}=this.selectNumber
+        this.props.jiaAsync(value*1,500)
     }
     render() {
-        return (
-            <div>
-                <h2>当前求和为：{this.props.he}</h2>
-                <button onClick={this.add}>点击加1</button>
+         return (            
+            <div>                
+                <h1>当前求和为：{this.props.count}</h1>
+                <select ref={c=>this.selectNumber=c}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>&nbsp;
+                <button onClick={this.increment}>+</button>&nbsp;
+                <button onClick={this.decrement}>-</button>&nbsp;
+                <button onClick={this.incrementIfOdd}>当前和为奇数再加</button>&nbsp;
+                <button onClick={this.incrementAsync}>异步加</button>&nbsp;
             </div>
         )
     }
 }
 
+const mapStateToProps = state => ({count:state})
+
 export default connect(
-    state => ({he:state}),
-    {add:createIncrementAction,}
+    state => ({count:state}),
+    {
+        jia:createIncrementAction,
+        jian:createDecrementAction,
+        jiaAsync:createIncrementAsyncAction,
+    }
 )(Count)
