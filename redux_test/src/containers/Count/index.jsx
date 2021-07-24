27df8081491409420ab317bda79363f3d81e2ba6
lygwys <lygwys@outlook.com>
2021-07-24 12:20:41 +0800

    2.引入connect,显示状态

--- redux_test/src/containers/Count/index.jsx
+++ redux_test/src/containers/Count/index.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
-export default class index extends Component {
+class Count extends Component {
     render() {
         return (
             <div>
-                <h2>当前求和为：？？？</h2>
+                <h2>当前求和为：{this.props.he}</h2>
             </div>
         )
     }
 }
+
+export default connect(
+    state => ({he:state}),
+    {}
+)(Count)
