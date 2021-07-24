e3b621b731a3ad4f3bf51b44ffdd3ea5464cb0f2
lygwys <lygwys@outlook.com>
2021-07-24 12:27:06 +0800

    3.引入action实现加1

--- redux_test/src/containers/Count/index.jsx
+++ redux_test/src/containers/Count/index.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
+import {createIncrementAction} from '../../redux/count_action'
 
 class Count extends Component {
+    add = ()=>{
+        this.props.add(1)
+    }
     render() {
         return (
             <div>
                 <h2>当前求和为：{this.props.he}</h2>
+                <button onClick={this.add}>点击加1</button>
             </div>
         )
     }
@@ -13,5 +18,5 @@ class Count extends Component {
 
 export default connect(
     state => ({he:state}),
-    {}
+    {add:createIncrementAction,}
 )(Count)
