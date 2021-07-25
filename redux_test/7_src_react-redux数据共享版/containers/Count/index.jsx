562eb879a6fb8897805be50dc66fb60e77ffea2a
lygwys <lygwys@outlook.com>
2021-07-25 15:44:11 +0800

    备份7_src_react-redux数据共享版

---
+++ redux_test/7_src_react-redux数据共享版/containers/Count/index.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {createIncrementAction,
+    createDecrementAction,
+    createIncrementAsyncAction
+} from '../../redux/actions/count'
+
+class Count extends Component {
+    state={carName:'奔驰c63'}
+
+    increment=()=>{
+        const {value}=this.selectNumber
+        this.props.jia(value*1)
+    }
+    decrement=()=>{
+        const {value}=this.selectNumber
+        this.props.jian(value*1)
+    }
+    incrementIfOdd=()=>{
+        const {value}=this.selectNumber
+        if (this.props.count % 2 !== 0) {
+            this.props.jia(value*1)
+        }
+    }
+    incrementAsync=()=>{
+        const {value}=this.selectNumber
+        this.props.jiaAsync(value*1,500)
+    }
+    render() {
+         return (            
+            <div> 
+                <h1>我是Count组件</h1>               
+                <h4>当前求和为：{this.props.count}，总人数为：{this.props.renshu}</h4>
+                <select ref={c=>this.selectNumber=c}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>&nbsp;
+                <button onClick={this.increment}>+</button>&nbsp;
+                <button onClick={this.decrement}>-</button>&nbsp;
+                <button onClick={this.incrementIfOdd}>当前和为奇数再加</button>&nbsp;
+                <button onClick={this.incrementAsync}>异步加</button>&nbsp;
+            </div>
+        )
+    }
+}
+
+export default connect(
+    state => ({count:state.he,renshu:state.rens.length}),
+    {
+        jia:createIncrementAction,
+        jian:createDecrementAction,
+        jiaAsync:createIncrementAsyncAction,
+    }
+)(Count)
---
+++ redux_test/7_src_react-redux数据共享版/containers/Person/index.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import {nanoid} from 'nanoid'
+import {connect} from 'react-redux'
+import {createAddPersonAction} from '../../redux/actions/person'
+
+ class Person extends Component {
+    addPerson=()=>{
+        const name = this.nameNode.value
+        const age = this.ageNode.value
+        const personObj = {id:nanoid(),name,age}
+        console.log(personObj)
+        this.props.jiaYiRen(personObj)
+        this.nameNode.value = ''
+        this.ageNode.value = ''
+    }
+    
+    render() {
+        console.log(this)
+        return (
+            <div>
+                <h1>我是Person组件,当前求和为：{this.props.he}</h1>
+                <input ref={c=>this.nameNode=c} type="text" placeholder="请输入名字" />
+                <input ref={c=>this.ageNode=c} type="text" placeholder="请输入年龄" />
+                <button onClick={this.addPerson}>添加</button>
+                <ul>
+                    {
+                        this.props.yiduiren.map((p)=>{
+                            return <li key={p.id}>名字：{p.name}年龄：{p.age}</li>
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default connect(
+    state => ({yiduiren:state.rens,he:state.he}),
+    {jiaYiRen:createAddPersonAction}
+)(Person)
