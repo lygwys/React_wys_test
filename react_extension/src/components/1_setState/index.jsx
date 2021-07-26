25ec04f29f3d50ed6089671e5669c6346c72e55c
lygwys <lygwys@outlook.com>
2021-07-26 11:31:56 +0800

    对象式的setState

--- react_extension/src/components/1_setState/index.jsx
+++ react_extension/src/components/1_setState/index.jsx
@@ -5,7 +5,10 @@ export default class Demo extends Component {
 
     add = () =>{
         const {count} = this.state
-        this.setState({count:count + 1})
+        this.setState({count:count + 1},()=>{
+            console.log('setState的回调函数输出是render后的值：',this.state.count)
+        })
+        console.log('setState后的输出，因为是异步的，还是原来的值：',this.state.count)
     }
     render() {
         return (
