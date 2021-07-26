7d6db4165de07c53e17564d34e39004d528f4202
lygwys <lygwys@outlook.com>
2021-07-26 20:55:39 +0800

    setCount第一种写法

--- react_extension/src/components/3_hooks/index.jsx
+++ react_extension/src/components/3_hooks/index.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
 
-export default class Demo extends Component {
-    state = {count:0}
+// export default class Demo extends Component {
+//     state = {count:0}
 
-    add = () => {
-        this.setState(state=>({count:state.count+1}))
+//     add = () => {
+//         this.setState(state=>({count:state.count+1}))
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h2>当前求和为{this.state.count}</h2>
+//                 <button onClick={this.add}>点我+1</button>
+//             </div>
+//         )
+//     }
+// }
+
+function Demo(){
+    const [count,setCount] = React.useState(0)
+    function add(){
+        setCount(count+1)
     }
-    render() {
-        return (
-            <div>
-                <h2>当前求和为{this.state.count}</h2>
-                <button onClick={this.add}>点我+1</button>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h2>当前求和为{count}</h2>
+            <button onClick={add}>点我+1</button>
+        </div>
+    )
 }
+
+export default Demo
