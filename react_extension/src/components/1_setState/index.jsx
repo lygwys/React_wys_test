4fda2aa1898650be5affe9983b3e8a302913dff2
lygwys <lygwys@outlook.com>
2021-07-26 11:20:33 +0800

    setState第一种写法，传对象

---
+++ react_extension/src/components/1_setState/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Demo extends Component {
+    state = {count:0}
+
+    add = () =>{
+        const {count} = this.state
+        this.setState({count:count + 1})
+    }
+    render() {
+        return (
+            <div>
+                <h1>当前求和为{this.state.count}</h1>
+                <button onClick = {this.add}>点我+1</button>
+            </div>
+        )
+    }
+}
