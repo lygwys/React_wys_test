fdd98e3d10a4bdb7c757971692351b10a8a6d01c
lygwys <lygwys@outlook.com>
2021-07-26 21:09:15 +0800

    用state Hook实现改名

--- react_extension/src/components/3_hooks/index.jsx
+++ react_extension/src/components/3_hooks/index.jsx
@@ -18,14 +18,24 @@ import React, { Component } from 'react'
 
 function Demo(){
     const [count,setCount] = React.useState(0)
+    const [name,setName] =React.useState('tom')
+
     function add(){
         //setCount(count+1)//setCount第一种写法
         setCount(count => count+1)//setCount第二种写法，函数形式，传入先前状态，返回新的状态
     }
+
+    function changeName(){
+        //setName(name => 'atguigu')
+        setName('jack')
+    }
+
     return (
         <div>
             <h2>当前求和为{count}</h2>
+            <h2>我的名字是：{name}</h2>
             <button onClick={add}>点我+1</button>
+            <button onClick={changeName}>点我改名</button>
         </div>
     )
 }
