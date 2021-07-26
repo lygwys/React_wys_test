a71ddc7db65bb6a90fa95fe522cb2eeaebfb807f
lygwys <lygwys@outlook.com>
2021-07-26 16:54:33 +0800

    使用懒加载

--- react_extension/src/components/2_lazyLoad/index.jsx
+++ react_extension/src/components/2_lazyLoad/index.jsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component,lazy,Suspense } from 'react'
 import {NavLink,Route} from 'react-router-dom'
-import About from './About'
-import Home from './Home'
+// import About from './About'
+// import Home from './Home'
+const Home = lazy(() => import('./Home'))
+const About = lazy(() => import('./About'))
 
 export default class Demo extends Component {
     render() {
@@ -24,8 +26,10 @@ export default class Demo extends Component {
                 <div className="col-xs-6">
                     <div className="panel">
                     <div className="panel-body">
+                        <Suspense fallback={<h1>Loading...</h1>}>
                             <Route path="/about" component={About}/>
-                            <Route path="/home" component={Home}/>                   
+                            <Route path="/home" component={Home}/>  
+                        </Suspense>                                             
                     </div>
                     </div>
                 </div>
