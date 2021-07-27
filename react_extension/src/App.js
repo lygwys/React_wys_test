abce09114532e6f3c092a58cd8b42566349ba96d
lygwys <lygwys@outlook.com>
2021-07-27 09:37:42 +0800

    使用div，页面显示所有层级div

--- react_extension/src/App.js
+++ react_extension/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import Demo from './components/3_hooks'
+import Demo from './components/4_Fragment'
 
 
 export default class App extends Component {
     render() {
         return (
-            <div>
+            <div name='1'>
                 <Demo/>
             </div>
         )
---
+++ react_extension/src/components/4_Fragment/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Demo extends Component {
+    render() {
+        return (
+            <div name='2'>
+                <h2>Fragment</h2>
+            </div>
+        )
+    }
+}
